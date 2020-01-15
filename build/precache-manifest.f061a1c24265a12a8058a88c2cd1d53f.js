f222c0bcc4ca7d015b66e71bd070198bb92dc4e8
German Attanasio <germanattanasio@gmail.com>
2020-01-15 16:02:47 -0500

    feat: use concurrently

---
+++ build/precache-manifest.f061a1c24265a12a8058a88c2cd1d53f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf03c9c860d69c64cb3f10d839a249a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e64f37ad14c64b84d378",
+    "url": "/static/css/main.fd406c70.chunk.css"
+  },
+  {
+    "revision": "2d161aa7b30bf384ed33",
+    "url": "/static/js/2.dee84cbf.chunk.js"
+  },
+  {
+    "revision": "5d03d7ab426f948560bd807db622e15b",
+    "url": "/static/js/2.dee84cbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e64f37ad14c64b84d378",
+    "url": "/static/js/main.0a667290.chunk.js"
+  },
+  {
+    "revision": "943ff24e949884b1b647",
+    "url": "/static/js/runtime-main.9f59d3bd.js"
+  }
+]);
