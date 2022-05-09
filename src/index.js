91cf128abcfe21b325409fbd6c30773b0e708edd
Claudia G Villanueva Gallegos <clgvg@mx1.ibm.com>
2022-05-09 12:52:06 -0500

    Improve the performance compilation from: [client] webpack compiled successfully in 78253 ms (around) to [client] webpack compiled successfully in 523 ms (around) for every change un html or scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/main.scss';
+import './styles/_index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
